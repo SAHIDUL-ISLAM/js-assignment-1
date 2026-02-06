9cf1a4317b3dd7ba3e77a0374af1d872926fff39
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-02-06 22:12:18 +0600

    problem 1 new price for eid sale solution

---
+++ Problem-01 New Price for Eid Sale.js
@@ -0,0 +1,15 @@
+
+function newPrice(currentPrice , discount ) {
+    if(typeof currentPrice!=='number' || typeof discount!=='number'){
+        return "Invalid";
+    }else if(discount<0 || discount>100){
+        return "Invalid";
+    }
+    
+    const discountPrice = (currentPrice*discount)/100;
+    const number = (currentPrice-discountPrice).toFixed(3);
+    return number;
+}
+const result = newPrice(1500,20);
+console.log(result);
+
